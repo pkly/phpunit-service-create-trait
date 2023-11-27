402ad560b91870ef0af57f018398a23d68ca1351
pkly <kakolp95@gmail.com>
2023-11-27 12:55:23 +0100

    Init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# phpunit-service-create-trait
+# PHPUnit Service Create Trait
 A helper trait for PHPUnit 10+ for easier creation of services with dependencies in unit testing
+
+## Usage
+
+In any of your PHPUnit test cases simply 
+
+```php
+class MyTestCase extends \PHPUnit\Framework\TestCase {
+    use \Pkly\ServiceMockHelperTrait;
+    
+    private AnyClass $service;
+    
+    public function setUp(): void {
+        $this->service = $this->createRealMockedServiceInstance(AnyClass::class);
+    }
+
+    public function testSomething(): void
+    {
+        $mock = $this->createMock(MyEntity::class);
+    
+        $this->getMockedService(EntityManagerInterface::class)
+            ->expects($this->once())
+            ->method('delete')
+            ->with($mock);
+            
+        $this->service->deleteSomething($mock);
+    }
+}
+```
+
+Any dependencies in the constructor as well as methods marked with Symfony's `#[Required]` attribute will be automatically plugged in with mocks.
+This allows you to write complex tests without wasting time updating your construct calls each time you modify something.
+
+### Okay, but what if I need to use something custom?
+
+Simply assign the proper parameter name in either `$constructor` or `$required` in the appropriate methods.
+That will use your object instead of creating one for you, keep in mind you cannot retrieve it via `$this->getMockedService()`.
+
+### Partial objects?
+
+Sure, works the same, just use `createRealPartialMockedServiceInstance` instead of `createRealMockedServiceInstance`, in that case you must
+also specify the methods to override in your mock. Returned instance is `T&MockObject`.
+
+### Tests? More examples?
+
+I'll add them shortly, for now this code is being used thoroughly in a few of the projects I work at and I grew tired of updating it
+across multiple repositories. It's also very simple, so I doubt anyone is going to complain.
+
+### Feature requests?
+
+Sure, hit me up with an issue if you wish.
---
+++ src/ServiceMockHelperTrait.php
@@ -0,0 +1,233 @@
+<?php
+
+namespace Pkly;
+
+use PHPUnit\Framework\MockObject\MockBuilder;
+use PHPUnit\Framework\MockObject\MockObject;
+
+trait ServiceMockHelperTrait
+{
+    /**
+     * @var array<class-string, array<class-string, MockObject>>
+     */
+    private array $mocks = [];
+
+    /**
+     * @param class-string<object> $class
+     *
+     * @return array{0: MockObject, 1: class-string}
+     */
+    private function __createMockedServiceParameter(
+        string $class,
+        \ReflectionParameter $parameter,
+        \ReflectionMethod $method
+    ): array {
+        if (null === ($type = $parameter->getType())) {
+            throw new \LogicException(
+                sprintf(
+                    'Cannot read type of parameter $%s in %s::%s',
+                    $parameter->getName(),
+                    $class,
+                    $method->getName()
+                )
+            );
+        }
+
+        if (method_exists($type, 'getTypes')) {
+            if (1 !== count($type->getTypes())) {
+                throw new \LogicException(
+                    sprintf(
+                        'Creating mocks for more than one time at a time are not supported at this time for $%s in %s::%s',
+                        $parameter->getName(),
+                        $class,
+                        $method->getName()
+                    )
+                );
+            }
+
+            $type = $type->getTypes()[0];
+        }
+
+        if ($type->isBuiltin()) {
+            throw new \LogicException(
+                sprintf(
+                    'Specify parameter $%s in %s::%s',
+                    $parameter->getName(),
+                    $class,
+                    $method->getName()
+                )
+            );
+        }
+
+        /**
+         * @var class-string<object> $final
+         *
+         * @noinspection PhpRedundantVariableDocTypeInspection
+         */
+        $final = $type->getName();
+
+        return [
+            $this->createMock($final),
+            $type->getName(),
+        ];
+    }
+
+    /**
+     * @param list<mixed> $definedParameters
+     *
+     * @return list<mixed>
+     */
+    private function __createAndGetMethodParams(
+        \ReflectionMethod $method,
+        array $definedParameters
+    ): array {
+        /** @var list<mixed> $params */
+        $params = [];
+
+        foreach ($method->getParameters() as $parameter) {
+            if (array_key_exists($parameter->getName(), $definedParameters)) {
+                $params[] = $definedParameters[$parameter->getName()];
+                continue;
+            }
+
+            [$mocked, $type] = $this->__createMockedServiceParameter($method->class, $parameter, $method);
+            $params[] = $mocked;
+
+            $this->mocks[$method->class][$type] = $mocked;
+        }
+
+        return $params;
+    }
+
+    /**
+     * @template T of object
+     *
+     * @param class-string<T>|string $class
+     * @param class-string<object>|null $service
+     *
+     * @return MockObject&T
+     *
+     * @phpstan-ignore-next-line
+     */
+    protected function getMockedService(
+        string $class,
+        string|null $service = null
+    ): MockObject {
+        if (null === $service) {
+            reset($this->mocks);
+            $service = key($this->mocks);
+        }
+
+        if (null === $service) {
+            throw new \LogicException('No services have been mocked yet by the trait');
+        }
+
+        if (null === ($object = ($this->mocks[$service][$class] ?? null))) {
+            throw new \LogicException(
+                sprintf(
+                    'Mocked class %s not found in %s',
+                    $class,
+                    $service
+                )
+            );
+        }
+
+        /** @var MockObject&T $object */
+        return $object;
+    }
+
+    /**
+     * @template T
+     *
+     * @param class-string<T> $class
+     * @param array<string, mixed> $constructor
+     * @param array<string, mixed> $required
+     *
+     * @return T
+     */
+    protected function createRealMockedServiceInstance(
+        string $class,
+        array $constructor = [],
+        array $required = []
+    ) {
+        try {
+            $reflection = new \ReflectionClass($class);
+            /** @phpstan-ignore-next-line */
+        } catch (\ReflectionException $e) {
+            throw new \LogicException('Failed to read class reflection, specify proper FQCN', previous: $e);
+        }
+
+        // reset instance in memory
+        $this->mocks[$class] = [];
+        $params = [];
+
+        if (null !== ($construct = $reflection->getConstructor())) {
+            $params = $this->__createAndGetMethodParams($construct, $constructor);
+        }
+
+        $service = new $class(...$params);
+
+        foreach ($reflection->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            if (empty($method->getAttributes(\Symfony\Contracts\Service\Attribute\Required::class))) {
+                continue;
+            }
+
+            $service->{$method->getName()}(...$this->__createAndGetMethodParams($method, $required));
+        }
+
+        return $service;
+    }
+
+    /**
+     * @template T
+     *
+     * @param class-string<T> $class
+     * @param list<string> $methods
+     * @param array<string, MockObject|object|scalar> $constructor
+     * @param array<string, MockObject|object|scalar> $required
+     *
+     * @return T&MockObject
+     */
+    protected function createRealPartialMockedServiceInstance(
+        string $class,
+        array $methods,
+        array $constructor = [],
+        array $required = []
+    ) {
+        try {
+            $reflection = new \ReflectionClass($class);
+            /** @phpstan-ignore-next-line */
+        } catch (\ReflectionException $e) {
+            throw new \LogicException('Failed to read class reflection, specify proper FQCN', previous: $e);
+        }
+
+        // reset instance in memory
+        $this->mocks[$class] = [];
+        $params = [];
+
+        if (null !== ($construct = $reflection->getConstructor())) {
+            $params = $this->__createAndGetMethodParams($construct, $constructor);
+        }
+
+        $service = (new MockBuilder($this, $class))
+            ->setConstructorArgs($params)
+            ->disableOriginalClone()
+            ->onlyMethods($methods)
+            ->getMock();
+
+        foreach ($reflection->getMethods(\ReflectionMethod::IS_PUBLIC) as $method) {
+            if (empty($method->getAttributes(\Symfony\Contracts\Service\Attribute\Required::class))) {
+                continue;
+            }
+
+            $service->{$method->getName()}(...$this->__createAndGetMethodParams($method, $required));
+        }
+
+        \PHPUnit\Event\Facade::emitter()->testCreatedPartialMockObject(
+            $class,
+            ...$methods,
+        );
+
+        return $service;
+    }
+}
